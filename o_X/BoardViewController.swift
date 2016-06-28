f9124ec7acc1e633f95a2946e7f9eb929222b1fb
jlbrill <jlbrill@umich.edu>
2016-06-28 21:31:14 +0200

    Final commit

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -5,6 +5,19 @@
 
 import UIKit
 
+/*class Location{
+    //let location:Int;
+    var currentVal:Character;
+    
+    init(){
+        currentVal = " ";
+    }
+    
+    func set_val(val_in:Character){
+        currentVal = val_in;
+    }
+}*/
+
 class BoardViewController: UIViewController {
 
     // Create additional IBOutlets here.
@@ -18,9 +31,15 @@ class BoardViewController: UIViewController {
     
     @IBAction func newGameButtonPressed(sender: UIButton) {
         print("New game button pressed.")
+        
+       // topLeft.titleLabel.text = "X"
     }
     
     // Create additional IBActions here.
+    @IBAction func boardTapped(sender: UIButton) {
+        print("board tapped \(sender.tag)")
+    }
+    
 
 }
 
