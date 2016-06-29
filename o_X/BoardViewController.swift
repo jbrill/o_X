7f0a1e1f41fd86e043bea220d2366111ed84d4b6
jlbrill <jlbrill@umich.edu>
2016-06-29 11:45:14 +0200

    data structures starting

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -5,23 +5,37 @@
 
 import UIKit
 
-/*class Location{
-    //let location:Int;
-    var currentVal:Character;
-    
-    init(){
-        currentVal = " ";
+/*class CreditCard{
+    //var num;
+}
+
+class User{
+    init(email_:String, password_:String?, firstName_:String, lastName_:String, middleName_:String?, creditCard_:CreditCard?){
+        email = email_;
+        password = password_;
+        firstName = firstName_;
+        middleName = middleName_;
+        CreditCard = creditCard_;
     }
     
-    func set_val(val_in:Character){
-        currentVal = val_in;
-    }
+    var email:String;
+    var password:String?;
+    var firstName:String;
+    var lastName:String;
+    var middleName:String?;
+    var creditCard:CreditCard?
 }*/
 
 class BoardViewController: UIViewController {
 
     // Create additional IBOutlets here.
+   // var creditCard:CreditCard;
+   // User user = ("Jbrill@gmail.com", "Hello", "Jason", "Brill", "Louis", creditCard);
     @IBOutlet weak var newGameButton: UIButton!
+    var myChars = [Character](count: 9, repeatedValue: " ")
+    var counter:Int = 0;
+    
+    //counter = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,14 +44,23 @@ class BoardViewController: UIViewController {
     }
     
     @IBAction func newGameButtonPressed(sender: UIButton) {
-        print("New game button pressed.")
-        
-       // topLeft.titleLabel.text = "X"
+        //print("New game button pressed.")
+        counter = 0;
     }
     
     // Create additional IBActions here.
     @IBAction func boardTapped(sender: UIButton) {
-        print("board tapped \(sender.tag)")
+        if(counter%2 == 0){
+            //print(sender.tag)
+            myChars[Int(sender.tag)] = "X";
+            sender.setAttributedTitle(NSAttributedString(string: "X"), forState: UIControlState.Normal)
+        } else {
+            myChars[sender.tag] = "O";
+            sender.setAttributedTitle(NSAttributedString(string: "O"), forState: UIControlState.Normal)
+        }
+        counter += 1;
+        
+        //print("board tapped \(sender.tag)")
     }
     
 
